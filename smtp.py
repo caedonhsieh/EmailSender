958b940fa699f79613c5f590fe704f8afc21211a
caedonhsieh <caedonhsieh@gmail.com>
2020-10-02 18:42:31 -0500

    finished cli

--- smtp.py
+++ smtp.py
@@ -1,28 +1,23 @@
 import smtplib
 import getmx
 
-DOMAIN = "google.com"
-HOST = getmx.get_host(DOMAIN)
-SENDER = "test@test.com"
-SENDNAME = "test tester"
-RECIPIENT = "ehhsiehfamily@gmail.com"
-SUBJECT = "subject line"
-MESSAGE = "this is a test message\nit is cool"
 
-fromLine = "From: {} <{}>\r\n".format(SENDNAME, SENDER)
-toLine = "To: <{}>\r\n".format(RECIPIENT)
-subjectLine = "Subject: {}\r\n".format(SUBJECT)
+def send(DOMAIN, SENDER, SENDNAME, RECIPIENT, SUBJECT, MESSAGE):
+    HOST = getmx.get_host(DOMAIN)
 
-msg = fromLine + toLine + subjectLine + MESSAGE
+    from_line = "From: {} <{}>\r\n".format(SENDNAME, SENDER)
+    to_line = "To: <{}>\r\n".format(RECIPIENT)
+    subject_line = "Subject: {}\r\n".format(SUBJECT)
 
-try:
-    server = smtplib.SMTP(host=HOST)
-    server.set_debuglevel(1)
-    server.sendmail(SENDER, RECIPIENT, msg)
-    server.quit()
-except smtplib.SMTPConnectError:
-    print('Error: Could not connect to server.')
-except smtplib.SMTPRecipientsRefused:
-    print('Error: Recipient refused.')
-except smtplib.SMTPDataError:
-    print('Error: Server refused data. Possible unauthenticated email.')
+    msg = from_line + to_line + subject_line + MESSAGE
+
+    try:
+        server = smtplib.SMTP(host=HOST)
+        server.sendmail(SENDER, RECIPIENT, msg)
+        server.quit()
+    except smtplib.SMTPConnectError:
+        print('Error: Could not connect to server.')
+    except smtplib.SMTPRecipientsRefused:
+        print('Error: Recipient refused.')
+    except smtplib.SMTPDataError:
+        print('Error: Server refused data. Possible unauthenticated email.')
