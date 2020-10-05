c503dd210345f0b03edb2269cd7a6e5ecfba70f7
caedonhsieh <caedonhsieh@gmail.com>
2020-10-04 20:15:07 -0500

    setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# make a python virtual environment if it doesn't exist yet
+if [ ! -d "env" ]; then
+	python3 -m venv env
+	source env/bin/activate
+	pip install -r requirements.txt
+	deactivate
+fi
+
+# add line to automatically use virtual environment for launcher.py and send_email.py
+foreach script ( launcher.py send_email.py )
+line=$(head -n 1 $script)
+SHEBANG="#!"
+if [ ${line:0:2} != $SHEBANG ]; then
+	(echo "#! $PWD/env/bin/python3" && cat $script) > temp && mv temp $script
+fi
+end
